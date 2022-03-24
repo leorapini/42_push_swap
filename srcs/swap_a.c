a47c81fdc29acb2c7049ce449704e8a20953c42b
Leo Rapini <leorapini@gmail.com>
2022-03-24 14:13:44 -0300

    add print function

--- srcs/swap_a.c
+++ srcs/swap_a.c
@@ -6,7 +6,7 @@
 /*   By: lpinheir <lpinheir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 14:00:14 by lpinheir          #+#    #+#             */
-/*   Updated: 2022/03/24 14:09:30 by lpinheir         ###   ########.fr       */
+/*   Updated: 2022/03/24 14:13:17 by lpinheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	swap_a(int *stack_a, size_t current_len)
 		temp_0 = stack_a[0];
 		stack_a[0] = stack_a[1];
 		stack_a[1] = temp_0;
+		printf("sa\n");
 	}
 	
 }
